52bfdde71eaa8844cbc8fb1e84cbe804e985941b
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-04-23 22:56:44 +1000

    add a NB model with non medical data

--- MedicalDataClassifier/med_predictor.py
+++ MedicalDataClassifier/med_predictor.py
@@ -5,8 +5,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 #Get the original notes
-raw_data = pd.read_csv('MedicalDataClassifier\demo_data.csv', usecols=[0])
-#get some no relevant data
-raw_data2 = pd.read_csv('MedicalDataClassifier\unrelated-data.csv', usecols=[0])
+df = pd.read_csv('MedicalDataClassifier\demo_data.csv', usecols=[0])
 
-print(raw_data)
+# 删除包含NaN值的行
+df = df.dropna()
+
+non_medical_data = [
+    "apple", "car", "tree", "laptop", "music", "soccer", "chocolate", "coffee",
+    "beach", "mountain", "shirt", "computer", "cellphone", "bicycle", "pencil",
+    "book", "movie", "television", "dog", "cat"
+]
+non_medical_df = pd.DataFrame(non_medical_data, columns=["raw"])
+non_medical_df["label"] = 0
+df = pd.concat([df, non_medical_df], ignore_index=True)
+X_train, X_test, y_train, y_test = train_test_split(df["raw"], df["label"], test_size=0.2, random_state=42)
+
+vectorizer = CountVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+nb_classifier = MultinomialNB()
+nb_classifier.fit(X_train_vec, y_train)
+
+y_pred = nb_classifier.predict(X_test_vec)
+
+print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
