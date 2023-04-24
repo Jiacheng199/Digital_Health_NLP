8ac8d002260bc234b8006c8733cb62526c61f37b
gaofenye <53753422+gaofenye@users.noreply.github.com>
2023-04-25 01:29:02 +1000

    Update mysql init file

--- src/Docker-Setup/Docker-Mysql/init.sql
+++ src/Docker-Setup/Docker-Mysql/init.sql
@@ -12,8 +12,8 @@ CREATE TABLE users (
 CREATE TABLE Mappings(
     id CHAR(36) NOT NULL,
     user_id CHAR(36) NOT NULL,
-    Title VARCHAR(255),
-    Descp VARCHAR(255),
+    username VARCHAR(255),
+    Commt VARCHAR(255),
     Editdate DATETIME,
     PRIMARY KEY (id),
     CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(id)
