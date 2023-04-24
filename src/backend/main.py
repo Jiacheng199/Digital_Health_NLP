83f25d03f03b98b9142c3055f994aa0d6dbc99b6
gaofenye <2080529087@qq.com>
2023-04-25 04:12:22 +1000

    update backend comments

--- src/backend/main.py
+++ src/backend/main.py
@@ -34,7 +34,6 @@ con = pymysql.connect(
     user='root',
     password='123456',
     db='mapping_system',
-    # cursorclass=pymysql.cursors.DictCursor
 )
 cursor=con.cursor()
 
@@ -76,26 +75,6 @@ def upload():
     uuid_str = str(uuid.uuid4())
     file.save(os.path.join(app.config["UPLOAD_FOLDER"], user_id, uuid_str+'.txt'))
     return jsonify({'message': 'File uploaded successfully','file_id':uuid_str}), 200
-    # if file:
-    #     file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-    #     return jsonify({'message': 'File uploaded successfully'}), 200
-    # else:
-    #     return jsonify({'message': 'File upload failed'}), 403
-# @app.route("/upload/<string:user_id>", methods=["POST"])
-# def upload_file(user_id):
-#     if "file" not in request.files:
-#         return "No file part", 400
-    
-#     file = request.files["file"]
-#     if file.filename == "":
-#         return "No selected file", 400
-#     save_path = os.path.join(app.config["UPLOAD_FOLDER"], user_id[1:-1])
-#     if not os.path.exists(save_path):
-#         os.makedirs(save_path)
-#     uuid_str = str(uuid.uuid4())
-#     file.save(os.path.join(app.config["UPLOAD_FOLDER"], user_id[1:-1], uuid_str+'.txt'))
-#     return jsonify({'message': 'File uploaded successfully','file_id':uuid_str}), 200
-
 
 
 
@@ -226,152 +205,13 @@ def deletemapping():
         writer = csv.writer(f)
         writer.writerows(rows)
     return jsonify({'message': 'Map deleted successfully'}), 200
-# @app.route("/getinfo",methods=["ge"])
-# def getinfo():
-#     data = request.get_json()
-#     tkn = data.get("tkn")
-#     username = jwt.decode(tkn, app.config['SECRET_KEY'],algorithms=["HS256"])['username']
-#     cursor.execute('SELECT * FROM users WHERE username=%s', username)
-#     result = cursor.fetchone()
-#     if result:
-#         return jsonify({'message': 'User found','username':result[1],'firstname':result[2],'lastname':result[3],'email':result[4]}), 200
-#     else:
-#         return jsonify({'message': 'User not found'}), 403
 
-
-# @app.route("/checklogin")
-# @token_required
-# def checklogin():
-#     if session.get('username') == request.args.get('token'):
-#         return jsonify({'message': 'Login successful'}), 200
-#     else:
-#         return jsonify({'message': 'Invalid credentials'}), 403
-    
-
-
-# @app.route("/upload", methods=["POST"])
-# def upload_file():
-#     if "file" not in request.files:
-#         return "No file part", 400
-    
-#     file = request.files["file"]
-#     if file.filename == "":
-#         return "No selected file", 400
-    
-#     _, file_extension = os.path.splitext(file.filename)
-#     if file_extension.lower() not in ALLOWED_EXTENSIONS:
-#         return "Unsupported file type", 400
-    
-#     file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-#     print(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-#     return "File uploaded successfully", 200
-
-# @app.route("/modify", methods=["POST"])
-# def modify_file():
-#     data = request.get_json()
-#     rawdata = data.get("modifiedData")
-#     # print(data)
-#     df = pandas.read_csv('modify.csv')
-#     processed_filename = data.get("output_filename")
-#     df_processed_file = pandas.read_csv(os.path.join(app.config["PROCESS_FOLDER"], processed_filename))
-#     for key, value in rawdata.items():
-#         matched_rows = df[df['raw'] == key]
-#         if len(matched_rows) == 0:
-#             df = df.append({"raw": key, "result": value}, ignore_index=True)
-#         else:
-#             df.loc[df['raw'] == key, 'result'] = value
-        
-#         df_processed_file.loc[df_processed_file['raw'] == key, 'result'] = value
-#     df_processed_file.to_csv(os.path.join(app.config["PROCESS_FOLDER"], processed_filename),index=False)
-#     df.to_csv('modify.csv', index=False)
-
-#     return jsonify({"message": "Successfully updated modify.csv"}), 200
-
-# @app.route("/process", methods=["POST"])
-# def process_file():
-#     data = request.get_json()
-#     filename = data.get("filename")
-#     if not filename:
-#         return "No filename provided", 400
-#     file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-#     if not os.path.exists(file_path):
-#         return "File not found", 404
-    
-#     map = map_sys(file_path, 'process_files/example.csv')
-#     processed_csv = map.mapping()
-#     # processed_csv = writing(result, 'example.csv')
-
-#     response_data = {"processed_csv": processed_csv, "output_filename": "example.csv"}
-#     return response_data,200
-
-# @app.route("/delete", methods=["POST"])
-# def delete_file():
-#     filename = request.form.get("filename")
-#     if not filename:
-#         return "No filename provided", 400
-
-#     file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    
-#     if not os.path.exists(file_path):
-#         return "File not found", 404
-
-#     try:
-#         os.remove(file_path)
-#         return "File deleted successfully", 200
-#     except Exception as e:
-#         print(f"Error deleting file: {e}")
-#         return "File deletion failed", 500
-
-# @app.route("/download/<userid>/<file_id>")
-# def download_file(userid, file_id):
 @app.route("/download", methods=["POST"])
 def download_file():
     file_id = request.json['file_id']
     userid = request.json['userid']
     path = os.path.join(app.config["PROCESS_FOLDER"], userid, file_id+'.csv')
     return send_file(path, as_attachment=True)
-
-
-
-# @app.route('/upload1', methods=['POST'])
-# def upload():
-#     if "files0" not in request.files:
-#         return "No file part", 400
-    # file = request.files["files"]
-    # if file.filename == "":
-    #     return "No selected file", 400
-    
-    # _, file_extension = os.path.splitext(file.filename)
-    # if file_extension.lower() not in ALLOWED_EXTENSIONS:
-    #     return "Unsupported file type", 400
-    # count = 0
-    # while True:
-    #     if "files"+str(count) not in request.files:
-    #         break
-    #     file = request.files["files"+str(count)]
-    #     file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-    #     count += 1
-    #     print(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-    # return "Uploaded successfully", 200
-
-# @app.route("/dropzone1", methods=["POST"])
-# def upload_file():
-#     if "file" not in request.files:
-#         return "No file part", 400
-
-#     file = request.files["file"]
-#     if file.filename == "":
-#         return "No selected file", 400
-    
-#     _, file_extension = os.path.splitext(file.filename)
-#     if file_extension.lower() not in ALLOWED_EXTENSIONS:
-#         return "Unsupported file type", 400
-    
-#     file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-#     print(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-#     return "File uploaded successfully", 200
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
