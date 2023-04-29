c6f506a31958116d48a5ac46feca1cd37878a345
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-04-29 13:21:54 +1000

    using nltk rake to generate the keywords from non-medical data

--- MedicalDataClassifier/med_predictor.py
+++ MedicalDataClassifier/med_predictor.py
@@ -4,11 +4,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.corpus import brown
+import gensim
+import re
+from rake_nltk import Rake
+
 nltk.download('brown')
 
 test = pd.read_csv('.\Medical_data\demo_data.csv')
@@ -32,17 +37,47 @@ def generate_non_medical_data():
     # Using the brown corpus
     non_medical_sentences = brown.sents(categories=['news', 'hobbies', 'lore', 'belles_lettres', 'government', 'learned', 'fiction', 'mystery', 'science_fiction', 'adventure', 'romance', 'humor'])
 
-    # Remove the stop words and punctuation
+    # Remove the stop words and punctuation, and join the words back into sentences
     stop_words = set(stopwords.words('english'))
-    non_medical_sentences = [[item for item in sentence if item not in stop_words] for sentence in non_medical_sentences]
-    non_medical_sentences = [[item for item in sentence if item.isalpha()] for sentence in non_medical_sentences]
+    non_medical_sentences = [' '.join([item for item in sentence if item not in stop_words and item.isalpha()]) for sentence in non_medical_sentences]
 
-    # Filter sentences with length between 1 and 3 words
-    filtered_sentences = [sentence for sentence in non_medical_sentences if 1 <= len(sentence) <= 3]
-    print(filtered_sentences)
-    return filtered_sentences
-generate_non_medical_data()
+    return non_medical_sentences
 
+
+def generate_keywords(sentences, num_keywords=3, min_words=1, max_words=3):
+    rake = Rake(min_length=min_words, max_length=max_words)
+    keywords_list = []
+    
+    for sentence in sentences:
+        rake.extract_keywords_from_text(sentence)
+        key_words = rake.get_ranked_phrases()[:num_keywords]
+        
+        # If no keywords found, return the most frequent word as a fallback
+        if not key_words:
+            words = word_tokenize(sentence)
+            if words:
+                most_frequent_word = max(set(words), key=words.count)
+                key_words.append(most_frequent_word)
+            else:
+                key_words.append('') # add an empty string if no words in the sentence
+
+        tokenized_key_words = word_tokenize(' '.join(key_words))
+        keywords_list.append(tokenized_key_words)
+
+        #remove all the keywords that length is bigger than 4
+        keywords_list = [item for item in keywords_list if len(item) <= 4]
+
+    return keywords_list
+
+non_medical_sentences = generate_non_medical_data()
+keywords_list = generate_keywords(non_medical_sentences)
+
+print(keywords_list[0:20])
+#get the longest keyword
+max_len = max([len(item) for item in keywords_list])
+print(max_len)
+
+print(keywords_list[0:50])
 def save_to_csv(data, file_name):
     with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
