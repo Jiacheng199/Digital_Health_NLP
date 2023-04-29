51b1c22d4495bf429b77cdda16caa75b7b216306
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-04-29 11:06:55 +1000

    Adding  WTO IDC 11 data for better prediction

--- MedicalDataClassifier/med_predictor.py
+++ MedicalDataClassifier/med_predictor.py
@@ -3,32 +3,26 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 #Get the original notes
-df = pd.read_csv('MedicalDataClassifier\demo_data.csv', usecols=[0])
+test = pd.read_csv('.\Medical_data\demo_data.csv')
+train = pd.read_excel('.\Medical_data\simpletabulation.xlsx')
 
-# 删除包含NaN值的行
-df = df.dropna()
+#print the columns
+print(train.columns)
+#take the 'Title' column only
+train['tokens'] = train['Title'].apply(lambda x: word_tokenize(x))
+train = train[['tokens']]
 
-non_medical_data = [
-    "apple", "car", "tree", "laptop", "music", "soccer", "chocolate", "coffee",
-    "beach", "mountain", "shirt", "computer", "cellphone", "bicycle", "pencil",
-    "book", "movie", "television", "dog", "cat"
-]
-non_medical_df = pd.DataFrame(non_medical_data, columns=["raw"])
-non_medical_df["label"] = 0
-df = pd.concat([df, non_medical_df], ignore_index=True)
-X_train, X_test, y_train, y_test = train_test_split(df["raw"], df["label"], test_size=0.2, random_state=42)
+#remove the stop words
+stop_words = set(stopwords.words('english'))
+train['tokens'] = train['tokens'].apply(lambda x: [item for item in x if item not in stop_words])
 
-vectorizer = CountVectorizer()
-X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
+#remove the punctuation
+train['tokens'] = train['tokens'].apply(lambda x: [item for item in x if item.isalpha()])
 
-nb_classifier = MultinomialNB()
-nb_classifier.fit(X_train_vec, y_train)
-
-y_pred = nb_classifier.predict(X_test_vec)
-
-print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-print("Confusion Matrix:")
-print(confusion_matrix(y_test, y_pred))
+print("Demo of traing data\n",train.head())
