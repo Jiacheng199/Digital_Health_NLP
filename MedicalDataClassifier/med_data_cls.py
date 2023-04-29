2bf8780cedfc89c02d334aca48d7b5155e96bd1a
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-04-29 14:41:35 +1000

    package the classifier to functino for futher import by other program, and update the usage and usage demo

--- MedicalDataClassifier/med_data_cls.py
+++ MedicalDataClassifier/med_data_cls.py
@@ -5,36 +5,42 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 
-train_data = pd.read_csv('./Medical_data/med_cls_train.csv')
+# Train the model and a simple evaluation
+def train_nb_classifier():
+    train_data = pd.read_csv('MedicalDataClassifier/Medical_data/med_cls_train.csv')
+    
+    #split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        train_data['text'], train_data['label'], test_size=0.1, random_state=42
+    )
 
-#using 9:1 ratio for train:test
-X_train, X_test, y_train, y_test = train_test_split(
-    train_data['text'], train_data['label'], test_size=0.1, random_state=42
-)
+    #vectorize the text first
+    vectorizer = CountVectorizer()
+    X_train_vec = vectorizer.fit_transform(X_train)
+    X_test_vec = vectorizer.transform(X_test)
 
-#vectorize the text
-vectorizer = CountVectorizer()
-X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
+    #using the Multinomial Naive Bayes classifier
+    clf = MultinomialNB()
+    clf.fit(X_train_vec, y_train)
 
-#using Multinomial Naive Bayes
-clf = MultinomialNB()
-clf.fit(X_train_vec, y_train)
+    y_pred = clf.predict(X_test_vec)
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    print('Accuracy: ', accuracy)
+    print('F1 score: ', f1)
 
-y_pred = clf.predict(X_test_vec)
-#accuracy and f1 score
-accuracy = accuracy_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred, average='weighted')
-print('Accuracy: ', accuracy)
-print('F1 score: ', f1)
+    return clf, vectorizer
 
+# Predict the label for a raw text
+def predict_medical_text(clf, vectorizer, raw_text):
+    raw_text_vec = vectorizer.transform([raw_text])
+    label = clf.predict(raw_text_vec)
+    return bool(label[0])
 
-#test on the demo data set
-demo_data = pd.read_csv('./Medical_data/demo_data.csv')
-demo_data_vec = vectorizer.transform(demo_data['raw'])
-#predict the label
-demo_data['label'] = clf.predict(demo_data_vec)
-#print the label 1 ratio
-print('Label 1 ratio: ', demo_data['label'].sum()/len(demo_data['label']))
+# Usage example
+if __name__ == '__main__':
+    classifier, text_vectorizer = train_nb_classifier()
+    test_text = "['heart', 'pain']"
+    result = predict_medical_text(classifier, text_vectorizer, test_text)
 
 
--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # Development for sprint2(future sptrint3 possible)
 
-This branch is for developing mapping data modification and login/register pages for now(US05 and US08).
+This branch is for developing mapping data modification,login/register pages for now(US05 and US08) and medical data classification using Naive Bayes classifier.
 
 This branch will be merged into the milestone branch using either the merge or manual copy method.
 
-## How to run the project
+## How to test the front-dev of this project
 Since we need to load the mock data to perform the mapping data modification testing, so one way to testing the current branch is to run 
 python code: `python -m http.server`; This will allow you to run the project on localhost:8000.
 
+## How to use the medical data classification function in other code
+First, ensure that you have the required Python libraries installed:
+
+`pip install pandas scikit-learn nltk rake_nltk numpy`
+
+Usage:
+Train the classifier using the training dataset:
+`classifier, text_vectorizer = train_nb_classifier()`
+
+Use the predict_medical_text function to predict whether the input text is medical-related:
+
+`result = predict_medical_text(classifier, text_vectorizer, test_text)`
+
+`print('Is the text medical-related? :', result)`
+
 ## NOTE
-This sprint is only a front-end project, but able to get the data from input. logical operation should not be implemented in this sprint branch,.
+Part of this sprint is only a front-end project, but able to get the data from input. logical operation should not be implemented in this sprint branch,.
