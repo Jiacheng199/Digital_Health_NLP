be7334e48611065ec3aff504e3aaf11240bebcf4
Jiacheng Wang <49028490+WangJiaCheng1999@users.noreply.github.com>
2023-04-30 10:27:03 +1000

    (AIS1.2) Change count vectorizer to tfidf vectorizer

--- MedicalDataClassifier/med_data_cls.py
+++ MedicalDataClassifier/med_data_cls.py
@@ -1,32 +1,33 @@
 #Description: This file is used to train the model and predict the label for the medical data, to classify whether the input raw text is legit medical text or not.
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 
+
 # Train the model and a simple evaluation
 def train_nb_classifier():
     train_data = pd.read_csv('MedicalDataClassifier/Medical_data/med_cls_train.csv')
-    
-    #split the data into training and testing sets
+
+    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(
         train_data['text'], train_data['label'], test_size=0.1, random_state=42
     )
-
-    #vectorize the text first
-    vectorizer = CountVectorizer()
+    # Vectorize the text first
+    vectorizer = TfidfVectorizer() 
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
 
-    #Use the Multinomial Naive Bayes classifier
+    # Use the Multinomial Naive Bayes classifier
     clf = MultinomialNB()
     clf.fit(X_train_vec, y_train)
 
-    #evaluate the model
+    # Evaluate the model
     evaluate_model(clf, vectorizer, X_test, y_test)
 
     return clf, vectorizer
+
 
 # Predict the label for a raw text
 def predict_medical_text(clf, vectorizer, raw_text):
