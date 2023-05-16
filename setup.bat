65fa3502402443a17c29d0080e196bc54dd9ad10
YXK <2581570217@qq.com>
2023-05-16 14:10:17 +1000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,24 +1,18 @@
 @echo off
 
 :RETRY
-REM 进入 src 目录
 cd src
 
-REM 登录到 Docker
 cmd /C docker login -u unimelbproject2023 -p 20232023di quay.io
 
 
-REM 使用 docker-compose 启动容器
 docker-compose up -d
 
-
-REM 在 "ontoserver" 容器中执行 index.sh 脚本
 docker exec ontoserver /index.sh
 
 echo Set up Succeeded
 
-REM 打开网页 https://localhost:3000
+
 start http://localhost:3000/login
 
-REM 退出批处理脚本
 pause
