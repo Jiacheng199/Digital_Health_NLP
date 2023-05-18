0bc0c7108c8e04dfe690b732f394a84b0590d501
YXK <2581570217@qq.com>
2023-05-18 23:24:31 +1000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,14 +5,17 @@ cd src
 
 cmd /C docker login -u unimelbproject2023 -p 20232023di quay.io
 
-
 docker-compose up -d
+
+echo progressing
+
+timeout /T 60
 
 docker exec ontoserver /index.sh
 
-echo Set up Succeeded
 
+echo Set up Succeeded
 
 start http://localhost:3000/login
 
-pause
+pause 
