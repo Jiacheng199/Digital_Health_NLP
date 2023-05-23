6bcc68b0966a29dd0b5df119004b61cc22acde7c
Caolang1019 <46103861+Caolang1019@users.noreply.github.com>
2023-05-24 00:14:44 +1000

    Update BIS BIS2.1

--- src/backend/read_ct.py
+++ src/backend/read_ct.py
@@ -4,9 +4,8 @@ from read_data import read_data
 def read_ct_result(uid):
     file_name = str(uid)+ '.csv'
     
+    # load raw file
     read = read_data()
     result = read.read_tmp_ct(file_name)
-    # print(result)
-
     os.remove(str(uid)+ '.csv')
     return result
