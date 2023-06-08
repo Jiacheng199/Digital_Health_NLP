2735458fb1b755e566e723cbd842e178a73fef1c
Caolang1019 <46103861+Caolang1019@users.noreply.github.com>
2023-06-08 16:29:45 +1000

    final

--- src/backend/ct_test.py
+++ src/backend/ct_test.py
@@ -10,9 +10,26 @@ def snomed_ct_dict(exa):
     # count = 0
     final_result = {}
     for i in raw.keys():
+        # count += 1
+        # if count % size != rank:
+        #     continue
         pre_data = raw[i]['processed']
         final_result[i] = ct.ct_string_process(pre_data)
 
 
     return final_result
+
+    # following for boost
+    # myuuid = uuid.uuid4()
+    # cmd = 'mpiexec -n 12 python snomed_ct.py ' + str(myuuid) + ' ' + str(exa)
+    # os.system(cmd)
+    # file_name = str(myuuid)+ '.csv'
+    
+    # read = read_data()
+    # result = read.read_tmp_ct(file_name)
+    # # print(result)
+
+    # os.remove(str(myuuid)+ '.csv')
+    # return result
+
 
